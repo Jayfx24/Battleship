620e638cba71af0e572cae8344fc6f35d1c1b833
Jayfx24 <176495720+Jayfx24@users.noreply.github.com>
2025-07-10 03:17:50 -0400

    refactor: add beforeEach

--- test/ship.test.js
+++ test/ship.test.js
@@ -1,19 +1,20 @@
 import { createShip } from '../src/modules/ship';
 
-console.log(createShip);
+
 
 
 describe('test createShip methods', () => {
-    
+    beforeEach(()=>{
+        ship = createShip('Destroyer', 3);
+
+    })
     test('increment hit count', () => {
-        const ship = createShip('Destroyer', 3);
         ship.hit();
         ship.hit();
         expect(ship.getHits()).toBe(2);
     });
 
     test('returns turn if ship is sunk', () => {
-        const ship = createShip('Destroyer', 3);
         ship.hit();
         ship.hit();
         ship.hit();
@@ -21,7 +22,6 @@ describe('test createShip methods', () => {
     });
 
     test('returns false if ship is not sunk', () => {
-        const ship = createShip('Destroyer', 3);
         ship.hit();
 
         expect(ship.isSunk()).toBe(false);
