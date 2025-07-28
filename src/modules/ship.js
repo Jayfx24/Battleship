40465ae94d741e1953e146c86d67cb52b00983aa
Jayfx24 <176495720+Jayfx24@users.noreply.github.com>
2025-07-28 04:07:08 -0400

    add: orientation to ship class

--- src/modules/ship.js
+++ src/modules/ship.js
@@ -1,12 +1,14 @@
-export function createShip(name, length){
+export function createShip(name, length, orientation = true){
     let hitCount = 0
    
     return{
         name,
         length,
+        orientation,
         hit: () => hitCount++,
         isSunk: () => hitCount === length ? true: false,
         getHits: () => hitCount
+
 
     }
 }
